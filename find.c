d1b34afd225d22ea7fb19b3bf065fabc0371de63
Tobi <TobiasJHolzinger@gmail.com>
2019-09-25 14:05:26 +0200

    Added insert

--- find.c
+++ find.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 #define SUM_H 1
 
 struct entry 
@@ -18,24 +19,55 @@ struct db
 
 };
 
+uint8_t hash(int function, char* input)
+{
+	return 1;
+}
 
-void *find(struct db *searchspace, char *entry_name)
+struct entry *find_entry(struct db *searchspace, char *name)
 {
 	struct entry *found;
-	uint16_t index = hash(SUM_H,entry_name);
+	uint8_t index = hash(SUM_H,name);
 	
-	if ( (found = db->list[index] ))
+	return searchspace->list[index];
+	
+}
+void *find(struct db *searchspace, char *name)
+{
+	struct entry *found;
+	uint8_t index = hash(SUM_H,name);
+	
+	if ( !searchspace->list[index])
 	{
 		printf("(find) : No entry found");
 		return NULL;
 	}
 
-	if (strcmp(found->name,entry_name))
+	printf("%p == %p = %d", searchspace->list[index]->name, name, strcmp(searchspace->list[index]->name, name));
+
+	if ((strcmp(searchspace->list[index]->name,name)))
 	{
-		return found->ptr;
+		printf("Found a match (%p)",searchspace->list[index]);
+		return searchspace->list[index]->ptr;
 	}
 	return NULL;
 	
 }
 
+void insert(struct db *searchspace, char *name, void *ptr)
+{
+	uint8_t index = hash(SUM_H,name);
+	struct entry *new_entry = (struct entry*) malloc(sizeof(struct entry*));
+	
+	
+	new_entry->name = (char *) malloc(strlen(name) * sizeof(char));
+	strcpy(new_entry->name,name);
+	new_entry->ptr = ptr;
 
+	new_entry->next = searchspace->list[index];
+	searchspace->list[index] = new_entry;
+	
+	printf("Inserted %s into the Database\n",(searchspace->list[index])->name);
+
+	return;
+}
--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "find.c"
 
+#define TEST_STR "shfkajsf"
 
 int main(int argc, char **args){
 
+	struct db *searchspace = (struct db *)malloc(sizeof(struct db));
+	char * name = (char *) malloc(strlen(TEST_STR) * sizeof(char));
+	strcpy(name, TEST_STR);
+	insert(searchspace,name,name);
+	void *got_name =  find(searchspace,name);
+	printf("... %p ...\n",got_name);
 
-
+	return 0;
 }
